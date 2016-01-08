ab4851a81fe9220905b4f17d44daf52c1d6298ff
Tom <wangyf@zouzhe.com>
2016-01-08 09:29:41 +0800

    oss

--- src/OssFilesystem.php
+++ src/OssFilesystem.php
@@ -1,109 +1,85 @@
 <?php
 /**
- * @link https://github.com/creocoder/yii2-flysystem
+ * @link https://github.com/okworld26/yii2-flysystem
  * @copyright Copyright (c) 2015 Alexander Kochetov
  * @license http://opensource.org/licenses/BSD-3-Clause
  */
 
 namespace okworld26\flysystem;
 
-use League\Flysystem\Adapter\Ftp;
+use Aliyun\Common\Models\ServiceOptions;
+use okworld26\AliyunOss\AliyunOssAdapter;
 use Yii;
 use yii\base\InvalidConfigException;
+use Aliyun\OSS\OSSClient;
 
 /**
- * FtpFilesystem
+ * OssFilesystem
  *
- * @author Alexander Kochetov <creocoder@gmail.com>
+ * @author Tom <okworld26@gmail.com>
  */
-class FtpFilesystem extends Filesystem
+class OssFilesystem extends Filesystem
 {
     /**
+     *
      * @var string
      */
-    public $host;
+    public $client_key;
     /**
      * @var integer
      */
-    public $port;
-    /**
-     * @var string
-     */
-    public $username;
+    public $client_secret;
     /**
      * @var string
      */
-    public $password;
+    public $endpoint;
     /**
-     * @var boolean
+     * @var string bucket name
      */
-    public $ssl;
-    /**
-     * @var integer
-     */
-    public $timeout;
-    /**
-     * @var string
-     */
-    public $root;
-    /**
-     * @var integer
-     */
-    public $permPrivate;
-    /**
-     * @var integer
-     */
-    public $permPublic;
-    /**
-     * @var boolean
-     */
-    public $passive;
-    /**
-     * @var integer
-     */
-    public $transferMode;
+    public $bucket;
+
+
+
 
     /**
      * @inheritdoc
      */
     public function init()
     {
-        if ($this->host === null) {
-            throw new InvalidConfigException('The "host" property must be set.');
+        if ($this->client_key === null) {
+            throw new InvalidConfigException('The "client_key" property must be set.');
         }
-
-        if ($this->root !== null) {
-            $this->root = Yii::getAlias($this->root);
+        if ($this->client_secret === null) {
+            throw new InvalidConfigException('The "client_secret" property must be set.');
         }
 
         parent::init();
     }
 
     /**
-     * @return Ftp
+     * @return Oss
      */
     protected function prepareAdapter()
     {
         $config = [];
 
-        foreach ([
-            'host',
-            'port',
-            'username',
-            'password',
-            'ssl',
-            'timeout',
-            'root',
-            'permPrivate',
-            'permPublic',
-            'passive',
-            'transferMode',
-        ] as $name) {
-            if ($this->$name !== null) {
-                $config[$name] = $this->$name;
-            }
-        }
+        /**
+         * 获取OSSClient实例用以访问OSS服务
+         *
+         * @param array $config Client的配置信息，可以包含下列Key：
+         * <li> Endpoint(必选) - OSS服务的Endpoint。必须以"http://"开头。
+         * <li> AccessKeyId(必选) - 访问OSS的Access Key ID。 </li>
+         * <li> AccessKeySecret(必选) -  访问OSS的Access Key Secret。</li>
+         *
+         * @return OSSClient
+         */
 
-        return new Ftp($config);
+        $config[ServiceOptions::ACCESS_KEY_ID] = $this->client_key;
+        $config[ServiceOptions::ACCESS_KEY_SECRET] = $this->client_secret;
+        $config[ServiceOptions::ENDPOINT] = $this->endpoint;
+
+        $ossClient = OSSClient::factory($config);
+
+        return new AliyunOssAdapter($ossClient,$this->bucket);
     }
 }
